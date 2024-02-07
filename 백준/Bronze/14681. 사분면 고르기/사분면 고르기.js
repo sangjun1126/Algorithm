245e86d0919c29992641ff49a4596261ad973708
JS_sangjun <jeongsangjun1234@gmail.com>
2024-02-07 14:04:07 +0900

    [Bronze V] Title: 사분면 고르기, Time: 140 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+const rl = readline.createInterface({input : process.stdin, output : process.stdout});
+
+let input = [];
+rl.on('line', function(line) {
+    input.push(parseInt(line));
+}).on('close', function() {
+    const a = input[0];
+    const b = input[1];
+    
+    if ((a > 0) && (b > 0)) {
+        console.log('1');
+    } else if ((a < 0) && (b > 0)) {
+        console.log('2');
+    } else if ((a < 0) && (b < 0)) {
+        console.log('3');
+    } else {
+        console.log('4');
+    }
+    
+    process.exit();
+})
