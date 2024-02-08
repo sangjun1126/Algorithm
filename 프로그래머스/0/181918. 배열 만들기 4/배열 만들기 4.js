5e6277644207283a8d317e5ea773bdf23105bcc3
JS_sangjun <jeongsangjun1234@gmail.com>
2024-02-09 01:00:12 +0900

    [level 0] Title: 배열 만들기 4, Time: 2.86 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    var stk = [];
+    let i = 0;
+    while ( i < arr.length) {
+        if(stk.length === 0) {
+            stk.push(arr[i])
+            i ++
+        } else {
+            if(stk[stk.length - 1] < arr[i]) {
+                stk.push(arr[i])
+                i ++
+            } else if (stk[stk.length - 1] >= arr[i]){
+                stk.pop()
+            }
+        }
+    }
+    return stk;
+}
