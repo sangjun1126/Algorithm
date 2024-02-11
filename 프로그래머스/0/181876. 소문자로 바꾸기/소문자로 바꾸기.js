d6ecbb897fab6a1b958d6f202432d0df8c514640
상준 <jeongsangjun1234@gmail.com>
2024-02-11 10:46:49 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.js
@@ -0,0 +1,12 @@
+function solution(myString) {
+    var answer = '';
+    // toLowercase = 소문자로 바꿔줍니다.
+    //myString == myString.toLowerCase();
+    //console.log(myString);
+    //var a = "aBcDeFg";
+    //a.toLowerCase();
+    //var b = "aaa";
+    //b.toLowerCase();
+    answer = myString.toLowerCase();
+    return answer;
+}
