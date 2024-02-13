9d23a7a1850ee4e04c912abd34cf106de83aa3eb
상준 <jeongsangjun1234@gmail.com>
2024-02-14 00:16:53 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,32 @@
+function solution(quiz) {
+    let ans = [];
+    
+    quiz.forEach((item) => {
+        const oneQuiz = item.split(" ");
+        
+        const X = Number(oneQuiz[0]);
+        const Y = Number(oneQuiz[2]);
+        
+        let calc = 0;
+        
+        if(oneQuiz[1] === "+"){
+            calc = X + Y;
+        }
+        
+        if(oneQuiz[1] === "-"){
+            calc = X - Y;
+        }
+        
+        const result = Number(oneQuiz[4]);
+        
+        if(calc === result){
+            ans.push("O");
+        }
+        
+        if(calc !== result){
+            ans.push("X");
+        }
+    })
+    
+    return ans;
+}
