0e718e49d09dabf94e035fef83a40eb865e7722d
상준 <jeongsangjun1234@gmail.com>
2024-02-15 00:30:21 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = '';
+    let arr = [];
+    for (let i =0; i <my_string.length; i++) {
+    if (my_string[i] === my_string[i].toUpperCase()) {
+        arr.push(my_string[i].toLowerCase());
+    } else {
+    arr.push(my_string[i].toUpperCase());
+    }
+}
+return arr.join("");
+}
