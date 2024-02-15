1a46f9b795bbddc53eda9087617e61b7a680cec4
상준 <jeongsangjun1234@gmail.com>
2024-02-16 02:30:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    var answer = 0;
+    let result = 0;
+
+for (let i=0; i< my_string.length; i++) {
+let tmp = 0;
+while (!Number.isNaN(Number(my_string[i]))) {
+tmp += my_string[i];
+i++
+}
+result += Number(tmp);
+}
+    return result;
+}
