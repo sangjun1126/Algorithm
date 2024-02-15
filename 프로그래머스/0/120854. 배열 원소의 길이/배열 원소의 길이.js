9b2547bf46eadc5dab9e2dbee0857b0542db7a7a
상준 <jeongsangjun1234@gmail.com>
2024-02-16 02:31:12 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,11 @@
+function solution(strlist) {
+    var answer = [];
+    for (let i=0; i<strlist.length; i++){
+        let temp = strlist[i].length;
+    answer.push(temp)
+    } 
+    return answer;
+}
+
+
+
