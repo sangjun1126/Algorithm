27472e85a25b94ddc7f3510e79516becde9e3df0
상준 <jeongsangjun1234@gmail.com>
2024-02-18 15:43:24 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+function solution(price) {
+    var answer = 0;
+if (price >= 100000 && price < 300000){
+answer = ~~(price*0.95);
+}else if(price >= 300000 && price < 500000){
+answer = ~~(price*0.9);
+}else if(price >= 500000){
+answer = ~~(price*0.8);
+}else {
+answer = price;
+}
+return answer;
+}
