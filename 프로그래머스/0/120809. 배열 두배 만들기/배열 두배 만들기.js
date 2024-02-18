ab8689b97228864ad8897d9eed20b6efc219e82f
상준 <jeongsangjun1234@gmail.com>
2024-02-18 15:45:53 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = [];
+    for (let i = 0; i<numbers.length; i++) {
+        answer.push(numbers[i] * 2);
+    }
+    return answer;
+}    
+
+
+
+
+//return answer = numbers.map(x => x*2)
