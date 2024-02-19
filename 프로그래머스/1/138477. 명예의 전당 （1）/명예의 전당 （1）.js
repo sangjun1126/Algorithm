e026082086a5f57ec685d4dbe77a94884867729c
상준 <jeongsangjun1234@gmail.com>
2024-02-19 14:35:09 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,13 @@
+function solution(k, score) {
+    var answer = [];
+    let list = [];
+    for (let i = 0; i < score.length; i++) {
+        list.push(score[i]);
+        list.sort((a,b) => b-a);
+        if (list.length > k) {
+            list.pop();
+        }
+        answer.push(list[list.length - 1])
+    }
+    return answer;
+}
