adb5dfe7a4b784a9c3d9207deacf96ecaca45375
상준 <jeongsangjun1234@gmail.com>
2024-02-21 01:54:20 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,6 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(x => Number(x));
+
+let max = Math.max(...input);
+
+console.log(max)
+console.log(input.indexOf(max) + 1)
