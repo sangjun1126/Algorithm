cb7e4bd285b041103b86f53b61abae6c7a9b0358
상준 <jeongsangjun1234@gmail.com>
2024-02-22 00:27:16 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    var answer = 0, sum = 0;
+    d.sort((a,b) => a -b);
+    
+    for (let i = 0; i < d.length; i++) {
+        answer++;
+        sum += d[i];
+        if (sum > budget) {
+            answer --
+        }
+    }
+    return answer;
+}
