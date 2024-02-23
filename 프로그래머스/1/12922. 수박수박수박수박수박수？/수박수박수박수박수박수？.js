86d4ecd874d7a105f5425e0a82c9612a67e84dd9
상준 <jeongsangjun1234@gmail.com>
2024-02-23 15:27:05 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = '';
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            answer += '박'
+        } else {
+            answer += '수'
+        }
+    }
+    return answer;
+}
