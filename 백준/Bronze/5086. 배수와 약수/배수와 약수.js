9e90f00e09cb0fec0a392bbab527062ebaa32db8
상준 <jeongsangjun1234@gmail.com>
2024-02-26 02:50:39 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+input.pop();
+const nums = input.map(v => v.split(' ').map(x => +x))
+
+nums.forEach(v => {
+  if (v[1] % v[0] == 0) {
+    console.log('factor')
+  } else if (v[0] % v[1] == 0) {
+    console.log('multiple')
+  } else {
+    console.log('neither')
+  }
+})
