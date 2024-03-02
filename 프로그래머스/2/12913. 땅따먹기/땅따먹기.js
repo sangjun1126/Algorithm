4dbd83e9f08572e906c7e529a1ed529b2e8fe803
상준 <jeongsangjun1234@gmail.com>
2024-03-02 15:49:01 +0900

    [level 2] Title: 땅따먹기, Time: 56.39 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,13 @@
+function solution(land) {
+  let answer
+  for(let i=1;i<land.length;i++){
+   land[i][0]+=Math.max(land[i-1][1],land[i-1][2],land[i-1][3])
+   land[i][1]+=Math.max(land[i-1][0],land[i-1][2],land[i-1][3])
+   land[i][2]+=Math.max(land[i-1][0],land[i-1][1],land[i-1][3])
+   land[i][3]+=Math.max(land[i-1][0],land[i-1][1],land[i-1][2])
+   }
+
+   answer=land[land.length-1]
+
+    return Math.max(...answer)
+}
