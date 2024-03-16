b11bafc7a2484defca7931c641c1a6a2b255352b
상준 <jeongsangjun1234@gmail.com>
2024-03-16 15:49:08 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.53 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  if (n === 1 || n === 2) return n;
+  const dp = Array(n).fill(0);
+  const mod = 1000000007;
+  dp[0] = 1;
+  dp[1] = 2;
+  for (let i = 2; i < n; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+  return dp[n - 1];
+}
