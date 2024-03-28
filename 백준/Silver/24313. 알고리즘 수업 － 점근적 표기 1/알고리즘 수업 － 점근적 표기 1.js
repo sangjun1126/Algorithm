477ec9803b352000fb4c9d54d36d3ab93e810eca
상준 <jeongsangjun1234@gmail.com>
2024-03-28 16:49:19 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync("/dev/stdin")
+.toString()
+.trim()
+.split("\n");
+
+const [a, b] = input[0].split(" ").map(Number);
+const c = parseInt(input[1]);
+const n0 = parseInt(input[2]);
+
+b <= (c-a)*n0 && c>=a ? console.log(1) : console.log(0)
