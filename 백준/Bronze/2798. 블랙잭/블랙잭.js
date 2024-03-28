26e18d90f23d5717af7bd9af4d4fbfd3863334f3
상준 <jeongsangjun1234@gmail.com>
2024-03-28 16:51:34 +0900

    [Bronze II] Title: 블랙잭, Time: 216 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const stringToNumber = (str) => {
+    return str.split(" ").map(Number);
+}
+
+const [N,M] = stringToNumber(input[0]);
+const cards = stringToNumber(input[1]);
+let max = 0;
+
+while(cards.length >= 3 && max !== M){
+    for(let i=1; i<cards.length-1; i++){
+     for(let j=i+1; j<cards.length; j++){
+      let sum = cards[0] + cards[i] + cards[j];
+      if(sum > max && sum <= M) max = sum;
+     }   
+   }
+    cards.shift();
+}
+
+console.log(max);
