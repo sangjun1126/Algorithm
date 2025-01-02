1103d05e52a8fcb2bd5ee32c253249970c325cb0
상준 <jeongsangjun1234@gmail.com>
2025-01-02 17:54:34 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,3 +1,9 @@
 function solution(array, n) {
-    return array.filter(v => v===n).length;
+    var answer = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === n) {
+            answer += 1
+        }
+    }
+    return answer;
 }
