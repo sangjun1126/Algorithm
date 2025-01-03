69cf538ce6add3f69e2cf6ee7a7ef1935ea472c8
상준 <jeongsangjun1234@gmail.com>
2025-01-03 15:33:28 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,12 @@
 function solution(n) {
     var answer = [];
-    for(let i = 1; i <= n; i++){
-    if(i%2 === 1){
-        answer.push(i);
+    let odd = [];
+    for (let i = 0; i <= n; i++) {
+        if (i % 2 === 0) {
+            odd.push(i) 
+        } else {
+            answer.push(i)
+        }
     }
-}
     return answer;
 }
