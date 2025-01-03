9a8c3a10f59504514d9020f91a0ab2b4906fc15d
상준 <jeongsangjun1234@gmail.com>
2025-01-03 15:16:08 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,5 +1,12 @@
 function solution(n, k) {
     var answer = 0;
-    answer = n * 12000 + k*2000 - Math.floor (n/10) * 2000;
+    let sol1 = n * 12000 + k * 2000;
+    // console.log(~~(n / 10))
+    if (n >= 10) {
+        answer = sol1 - (Math.floor(n/10) * 2000);
+    } else {
+        return sol1;
+    }
+    // console.log(sol1 - ~~(n/10))
     return answer;
 }
