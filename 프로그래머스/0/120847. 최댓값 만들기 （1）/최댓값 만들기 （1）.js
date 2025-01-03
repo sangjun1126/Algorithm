0d06ffaaed29c690eafbc1639890943560c7c987
상준 <jeongsangjun1234@gmail.com>
2025-01-03 15:30:13 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,6 +1,10 @@
 function solution(numbers) {
     var answer = 0;
-    var arr = numbers.sort((a, b) => b - a)
-    console.log(arr);
-    return numbers[0]*numbers[1];
+    let sorted_arr = numbers.sort((a,b) => a-b);
+    // console.log(sorted_arr)
+    // console.log(sorted_arr[sorted_arr.length]) == undefined
+    // console.log(sorted_arr.lengt-1h)
+    // console.log(sorted_arr[sorted_arr.length-1])
+    answer = sorted_arr[sorted_arr.length -1] * sorted_arr[sorted_arr.length -2]
+    return answer;
 }
