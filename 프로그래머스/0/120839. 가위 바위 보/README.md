6e00f44a1d085ee0032b1d3308c34a0e40a078d6
상준 <jeongsangjun1234@gmail.com>
2025-01-06 14:38:52 +0900

    [level 0] Title: 가위 바위 보, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/README.md
+++ 프로그래머스/0/120839. 가위 바위 보/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가위 바위 보 - 120839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.14 ms
+메모리: 33.5 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 2:38:38
+2025년 01월 06일 14:38:50
 
 ### 문제 설명
 
--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,14 +1,14 @@
 function solution(rsp) {
     var answer = '';
-    for (let i of rsp){
-        if (i === '0'){
-            answer += '5'
-        }else if (i === '2'){
-            answer += '0'
-        }else if (i ==='5'){
-            answer += '2'
+    let sp_rsp = rsp.split('');
+    for (let i = 0; i < sp_rsp.length; i++) {
+        if (sp_rsp[i] === "2") {
+            answer += "0"
+        } else if (sp_rsp[i] === "0") {
+            answer += "5"
+        } else {
+            answer += "2"
         }
     }
     return answer;
 }
-// map으로 전개
