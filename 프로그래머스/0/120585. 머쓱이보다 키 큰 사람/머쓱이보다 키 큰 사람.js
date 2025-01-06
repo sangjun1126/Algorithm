cd7dbff4a5e53e9b0f18e160b97996a4c0020b66
상준 <jeongsangjun1234@gmail.com>
2025-01-06 14:42:49 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,4 +1,11 @@
 function solution(array, height) {
     var answer = 0;
-    return array.filter(v => v >height).length
+    let sort_arr = array.sort((a,b) => a-b);
+    // console.log(sort_arr)
+    for (let i = 0; i < sort_arr.length; i++) {
+        if (sort_arr[i] > height) {
+            answer += 1
+        }
+    }
+    return answer;
 }
